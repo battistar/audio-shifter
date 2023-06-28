01b3cfdaeffab6c6bc1cdf64b07f071807894611
Samuele Battistella <battistar92@gmail.com>
2023-06-28 11:45:33 +0200

    Add system preference theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Container } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
 import PlaybackButtons from './components/PlaybackButtons';
 import PlaybackEffects from './components/PlaybackEffects';
 import { usePlayback } from './store';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 const App = (): JSX.Element => {
   const { setIsPlaying, setIsRepeating, isPlaying, isRepeating, setPitch, setSpeed, pitch, speed } = usePlayback();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   const handlePlayClick = useCallback(() => {
     setIsPlaying(!isPlaying);
@@ -30,20 +41,23 @@ const App = (): JSX.Element => {
   );
 
   return (
-    <Container maxWidth="md">
-      <PlaybackButtons
-        onPlayClick={handlePlayClick}
-        onRepeatClick={handleRepeatClick}
-        isPlaying={isPlaying}
-        isRepeating={isRepeating}
-      />
-      <PlaybackEffects
-        onPitchChange={handlePitchChange}
-        onSpeedChange={handleSpeedChange}
-        pitch={pitch}
-        speed={speed}
-      />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <PlaybackButtons
+          onPlayClick={handlePlayClick}
+          onRepeatClick={handleRepeatClick}
+          isPlaying={isPlaying}
+          isRepeating={isRepeating}
+        />
+        <PlaybackEffects
+          onPitchChange={handlePitchChange}
+          onSpeedChange={handleSpeedChange}
+          pitch={pitch}
+          speed={speed}
+        />
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CssBaseline } from '@mui/material';
 import { DataProvider } from './store/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CssBaseline />
     <DataProvider>
       <App />
     </DataProvider>
