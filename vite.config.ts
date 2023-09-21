2fd405f7ab391025e29a5c92193092514c9758fe
Samuele Battistella <battistar92@gmail.com>
2023-09-21 10:59:03 +0200

    Add base url to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import rollupNodePolyfillsPlugin from 'rollup-plugin-polyfill-node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'audio-shifter',
   plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
